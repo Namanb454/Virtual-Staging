a3970b6509e94b9f6b9d62446faed980bcb886a2
Naman <namanb454@gmail.com>
2024-03-23 15:59:23 +0530

    new commit

--- src/landingpage/Pricing.js
+++ src/landingpage/Pricing.js
@@ -54,6 +54,28 @@ const Pricing = () => {
     };
 
 
+    const pricing_data = [
+        {
+            'title': 'Virtual Staging with AI',
+            'method': 'Our Software',
+            'time': 'Seconds',
+            'price': 'Rs 100 / image'
+        },
+        {
+            'title': 'Virtual Staging',
+            'method': 'Humans using photoshop',
+            'time': 'Days',
+            'price': 'Rs 500 / image'
+        },
+        {
+            'title': 'Physical Staging',
+            'method': 'Physically staging a home with furniture',
+            'time': 'Weeks',
+            'price': 'Rs 1000 / image'
+        },
+    ]
+
+
     return (
         <div>
             <motion.section
@@ -62,9 +84,9 @@ const Pricing = () => {
                 initial='hidden'
                 animate='show'
                 id='pricing' className="scroll-smooth body-font overflow-hidden">
-                <div className="container px-5 xl:py-24 py-14 mx-auto">
+                <div className="container px-5 py-[10vh] mx-auto">
                     <motion.div
-                        className="flex flex-col text-center w-full xl:mb-20 mb-10">
+                        className="flex flex-col text-center w-full mb-[5vh]">
 
                         <motion.h1
                             variants={item}
@@ -77,102 +99,50 @@ const Pricing = () => {
 
                     </motion.div>
 
-                    <div className="flex flex-wrap -m-4">
-                        <motion.div
-                            variants={item}
-                            className="p-4 xl:w-1/3 2xl:h-[60vh] xl:h-[65vh] md:w-1/2 w-full px-10 ">
-                            <div className="bg-[#FBFBFB] h-full p-6 rounded-3xl py-10 shadow-md shadow-gray-600 hover:border-[#222121] border-2 relative overflow-hidden">
-                                <h1 className="2xl:text-4xl xl:text-3xl font-semibold text-left text-[#222121] leading-none pb-4 mb-4 border-b border-gray-200">
-                                    <h1>Basic Photoshoot</h1>
-                                    {/* <h3 className="2xl:text-lg xl:text-base py-2 font-medium text-[#222121]">1500 rupees/month</h3> */}
-                                </h1>
-                                <h2 className='text-2xl text-left text-black my-5'>Includes:</h2>
-                                <p className="flex items-center 2xl:text-lg mb-2">
-                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center text-black font-bold rounded-full flex-shrink-0">
-                                        {/* <MdArrowForward /> */}
-                                    </span>
-                                    <span className='font-bold'>10&nbsp;</span>Images
-                                </p>
-                                <p className="flex items-center 2xl:text-lg mb-2">
-                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center text-black font-bold rounded-full flex-shrink-0">
-                                        {/* <MdArrowForward /> */}
-                                    </span>
-                                    <span className='font-bold'>24&nbsp;</span>hour processing time
-                                </p>
+                    {/* <!-- Pricing --> */}
+                    <div className="mx-auto">
+                        {/* Grid */}
+                        <div className="">
+                            <div className="flex lg:items-center space-x-[5vw] mx-auto w-fit">
 
-                                <a href='#contact' className="flex items-center 2xl:mt-10 2xl:text-lg text-black border-2 border-[#222121] hover:text-white hover:bg-gray-200 py-2 px-4 w-full focus:outline-none rounded">Contact Us
-                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
-                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
+                                {pricing_data.map((data, index) => {
+                                    return (
+                                        <div className="w-[25vw] h-[60vh] text-center text-[#081d27] rounded-2xl shadow-lg shadow-[#081d27]">
+                                            <div className="py-[4vh] font-[Modeka] tracking-wide font-bold">
+                                                <h4 className="text-[2vw] ">{data.title}</h4>
+                                            </div>
+
+                                            <div className="lg:py-[2vh] font-[astro] tracking-widest">
+                                                <span className="font-bold text-[2vw] ">
+                                                    {data.price}
+                                                </span>
+                                            </div>
+
+                                            <div className=" flex justify-center py-[3.5vh] font-[modeka] font-bold">
+                                                <ul className="space-y-[2vh] text-center text-[1.5vw]">
+                                                    <li className="">
+                                                        {data.time}
+                                                    </li>
+                                                    <li className="">
+                                                        {data.method}
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <div className="p-[2vw] font-[astro]">
+                                                <a className="py-3 px-4 w-full inline-flex justify-center items-center gap-x-2 text-[1vw] font-semibold rounded-lg border border-blue-600 text-blue-600 hover:border-blue-500 hover:text-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:border-blue-500 dark:text-blue-500 dark:hover:text-blue-400 dark:hover:border-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/">
+                                                    Sign up
+                                                </a>
+                                            </div>
+                                        </div>
+                                    )
+                                })}
                             </div>
-                        </motion.div>
-
-
-                        <motion.div
-                            variants={item}
-                            className="p-4 xl:w-1/3 2xl:h-[60vh] xl:h-[65vh] md:w-1/2 w-full px-10 ">
-                            <div className="bg-gradient-to-r from-[#537FEF] to-[#D1C8FFBA] h-full p-6 rounded-3xl py-10 hover:border-[#222121] shadow-md shadow-gray-600 border-gray-200 border-2  relative overflow-hidden">
-                                <h1 className="2xl:text-4xl xl:text-3xl font-semibold text-left text-[#222121] leading-none pb-4 mb-4 border-b border-gray-200">
-                                    <h1>Standard Photoshoot</h1>
-                                    {/* <h3 className="2xl:text-lg xl:text-base py-2 font-medium text-[#222121]">2000 rupees/month</h3> */}
-                                </h1>
-                                <h2 className='text-2xl text-left text-black my-5'>Includes:</h2>
-                                <p className="flex items-center 2xl:text-lg mb-2">
-                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center text-black font-bold rounded-full flex-shrink-0">
-                                        {/* <MdArrowForward /> */}
-                                    </span>
-                                    <span className='font-bold'>50&nbsp;</span>Images
-                                </p>
-                                <p className="flex items-center 2xl:text-lg mb-2">
-                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center text-black font-bold rounded-full flex-shrink-0">
-                                        {/* <MdArrowForward /> */}
-                                    </span>
-                                    <span className='font-bold'>48&nbsp;</span>hour processing time
-                                </p>
-
-                                <a href='#contact' className="flex items-center 2xl:mt-10 2xl:text-lg text-black border-2 border-[#222121] hover:text-white hover:bg-gray-200 py-2 px-4 w-full focus:outline-none rounded">Contact Us
-                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
-                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-                            </div>
-                        </motion.div>
-
-
-                        <motion.div
-                            variants={item}
-                            className="p-4 xl:w-1/3 2xl:h-[60vh] xl:h-[65vh] md:w-1/2 w-full px-10 ">
-                            <div className="bg-white h-full p-6 rounded-3xl py-10 shadow-md shadow-gray-600 hover:border-[#222121] border-2 relative overflow-hidden">
-                                <h1 className="2xl:text-4xl xl:text-3xl font-semibold text-left text-[#222121] leading-none pb-4 mb-4 border-b border-gray-200">
-                                    <h1>Premium Photoshoot</h1>
-                                    {/* <h3 className="2xl:text-lg xl:text-base py-2 font-medium text-[#222121]">2500 rupees/month</h3> */}
-                                </h1>
-                                <h2 className='text-2xl text-left text-black my-5'>Includes:</h2>
-                                <p className="flex items-center 2xl:text-lg mb-2">
-                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center text-black font-bold rounded-full flex-shrink-0">
-                                        {/* <MdArrowForward /> */}
-                                    </span>
-                                    <span className='font-bold'>100&nbsp;</span>Images
-                                </p>
-                                <p className="flex items-center 2xl:text-lg mb-2">
-                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center text-black font-bold rounded-full flex-shrink-0">
-                                        {/* <MdArrowForward /> */}
-                                    </span>
-                                    <span className='font-bold'>72&nbsp;</span>hour processing time
-                                </p>
-
-                                <a href='#contact' className="flex items-center 2xl:mt-10 2xl:text-lg text-black border-2 border-[#222121] hover:text-white hover:bg-gray-200 py-2 px-4 w-full focus:outline-none rounded">Contact Us
-                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
-                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-                            </div>
-
-                        </motion.div>
-
+                        </div>
+                        {/* End Grid */}
 
                     </div>
+
+                    {/* <!-- End Pricing --> */}
                 </div>
             </motion.section>
         </div>
